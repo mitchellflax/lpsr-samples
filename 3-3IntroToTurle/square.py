4237b347dd778b0c844bf13650df19bfda0a91d1
Mitch Flax <mitchell.flax@gmail.com>
2015-12-14 15:14:19 -0800

    Change buzz to rocky.

--- 3-3IntroToTurle/square.py
+++ 3-3IntroToTurle/square.py
@@ -3,13 +3,13 @@
 import turtle
 
 # make our turtle
-buzz = turtle.Turtle()
+rocky = turtle.Turtle()
 
-# buzz makes a square
+# rocky makes a square
 lines = 0
 while lines < 4:
-	buzz.forward(150)
-	buzz.left(90)
+	rocky.forward(150)
+	rocky.left(90)
 	lines = lines + 1
 
 # wait to exit until I've clicked
