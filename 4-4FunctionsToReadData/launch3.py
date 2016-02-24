a7aa2e98261e7afcba7167307bfa05747ef9ce8c
Mitch Flax <mitchell.flax@gmail.com>
2016-02-24 10:55:03 -0800

    Created split() launch.

---
+++ 4-4FunctionsToReadData/launch3.py
@@ -0,0 +1,12 @@
+my_string = "She sells seashells by the seashore."
+
+my_tokens = my_string.split()
+
+print(my_tokens[0])
+print(my_tokens[1])
+print(my_tokens[5])
+print(my_tokens[0] + ' ' + my_tokens[1] + ' ' + my_tokens[5])
+
+
+print("Here's all of my_tokens:")
+print(my_tokens)
