cf9e5d1f102c2dc78576f586bb6373b469e7f69d
Mitch Flax <mitchell.flax@gmail.com>
2016-05-16 18:16:35 -0700

    Starting PS 9

---
+++ 6-3CaesarsCipher/applyCipher.py
@@ -0,0 +1,20 @@
+# applyCipher.py
+# A program to encrypt/decrypt user text
+# using Caesar's Cipher
+# 
+# Author: mflax [at] leadps.org
+
+# makes a mapping of encoded alphabet to decoded alphabet
+# arguments: key
+# returns: dictionary of mapped letters
+def createDictionary(key):
+
+	# placeholder
+	return {}
+
+# gets the encrypted message from the user
+# arguments: none
+# returns: encoded message
+def getMessage():
+	pass
+
